2f81f3ad7a19397362951758a78b272eec292bee
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-09-12 15:50:38 +0700

    Autofix tw classes

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,29 +1,29 @@
-import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
-import { cva, type VariantProps } from "class-variance-authority";
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-1 whitespace-nowrap rounded-lg px-5 py-2 text-sm font-semibold ring-offset-background transition focus:outline-none focus:ring-1 focus:ring-foreground focus:ring-offset-2 focus:ring-offset-black focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2  disabled:pointer-events-none disabled:opacity-50",
+  'inline-flex items-center justify-center gap-1 whitespace-nowrap rounded-lg px-5 py-2 text-sm font-semibold ring-offset-background transition focus:outline-none focus:ring-1 focus:ring-foreground focus:ring-offset-2 focus:ring-offset-black focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2  disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
         default:
-          "bg-lighter hover:bg-lighter/75 border border-border text-lighter-foreground hover:text-lightest-foreground",
-        light: "bg-foreground hover:bg-foreground/80 text-background",
-        dark: "bg-background hover:bg-background/80 text-foreground border-foreground border",
+          'bg-lighter hover:bg-lighter/75 border border-border text-lighter-foreground hover:text-lightest-foreground',
+        light: 'bg-foreground hover:bg-foreground/80 text-background',
+        dark: 'bg-background hover:bg-background/80 text-foreground border-foreground border',
       },
       size: {
-        default: "h-9 px-10 py-3",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        default: 'h-9 px-10 py-3',
+        sm: 'h-8 rounded-md px-3 text-xs',
+        lg: 'h-10 rounded-md px-8',
+        icon: 'size-9',
       },
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
+      variant: 'default',
+      size: 'default',
     },
   },
 );
@@ -36,7 +36,7 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button";
+    const Comp = asChild ? Slot : 'button';
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
@@ -46,6 +46,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     );
   },
 );
-Button.displayName = "Button";
+Button.displayName = 'Button';
 
 export { Button, buttonVariants };
