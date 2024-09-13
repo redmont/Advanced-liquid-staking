dd7c8e34222a0529d6c98748e35cd8bbe6318a41
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-09-13 11:50:10 +0700

    Remove next-auth

--- next.config.js
+++ next.config.js
@@ -2,16 +2,16 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
 const config = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
-    config.externals.push("pino-pretty", "lokijs", "encoding");
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
 };
--- src/lib/dynamic.ts
+++ src/lib/dynamic.ts
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
-export * from "@dynamic-labs/sdk-react-core";
-export * from "@dynamic-labs/ethereum";
+export * from '@dynamic-labs/sdk-react-core';
+export * from '@dynamic-labs/ethereum';
