5e4a4dc2c4788cd9a993a3c38c32a16f5628c973
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-10-22 17:22:01 +0700

    Fix build but this time for realsies

--- apps/realbet/next.config.js
+++ apps/realbet/next.config.js
@@ -1,4 +1,6 @@
+import path from 'path';
 import { withSentryConfig } from '@sentry/nextjs';
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
