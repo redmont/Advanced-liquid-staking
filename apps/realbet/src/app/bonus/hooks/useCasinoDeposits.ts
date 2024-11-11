f412ea521d4804bd4c336c21469882bac4de27be
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-11-12 01:30:03 +0800

    patch

--- apps/realbet/src/app/bonus/hooks/useCasinoDeposits.ts
+++ apps/realbet/src/app/bonus/hooks/useCasinoDeposits.ts
@@ -6,9 +6,11 @@ import { flatten } from 'lodash';
 import { useMemo } from 'react';
 import { isAddress } from 'viem';
 import { useWalletAddresses } from '@/hooks/useWalletAddresses';
+import { useSolsDeposits } from '../utils/solanaDepositChecker';
 
 const POINTS_PER_USD_DEPOSITED = 1;
 const POINT_THRESHOLD = 100;
+type Casino = 'rollbit' | 'shuffle' | 'bcgame' | 'betfury';
 
 const calculateDepositScore = (depositUSDValue: number) =>
   Math.floor(depositUSDValue / POINT_THRESHOLD) *
@@ -17,6 +19,7 @@ const calculateDepositScore = (depositUSDValue: number) =>
 
 export const useCasinoDeposits = () => {
   const { addresses: userWalletAddresses } = useWalletAddresses();
+  const solanaDeposits = useSolsDeposits();
 
   const evmAddresses = useMemo(
     () => userWalletAddresses.filter((addr) => isAddress(addr)),
@@ -38,35 +41,33 @@ export const useCasinoDeposits = () => {
     refetchOnWindowFocus: false,
   });
 
-  const amounts = useMemo(
-    () =>
-      deposits.isSuccess && deposits.data.length > 0
-        ? deposits.data.reduce(
-            (acc, deposit) =>
-              deposit.value !== null && deposit.price !== null
-                ? {
-                    ...acc,
-                    [deposit.casino]: {
-                      deposited:
-                        acc[deposit.casino].deposited +
-                        deposit.value * deposit.price,
-                      score:
-                        acc[deposit.casino].score +
-                        calculateDepositScore(deposit.value * deposit.price),
-                    },
-                  }
-                : acc,
-            {
-              rollbit: { deposited: 0, score: 0 },
-              shuffle: { deposited: 0, score: 0 },
-            },
-          )
-        : {
-            rollbit: { deposited: 0, score: 0 },
-            shuffle: { deposited: 0, score: 0 },
-          },
-    [deposits.data, deposits.isSuccess],
-  );
+  const amounts = useMemo(() => {
+    const initialAmounts: Record<Casino, { deposited: number; score: number }> = {
+      rollbit: { deposited: 0, score: 0 },
+      shuffle: { deposited: 0, score: 0 },
+      bcgame: { deposited: 0, score: 0 },
+      betfury: { deposited: 0, score: 0 },
+    };
+
+    Object.entries(solanaDeposits).forEach(([casino, solData]) => {
+      const casinoKey = casino as Casino;
+      initialAmounts[casinoKey].deposited += solData.deposited;
+      initialAmounts[casinoKey].score += calculateDepositScore(solData.deposited);
+    });
+
+    if (deposits.isSuccess && deposits.data.length > 0) {
+      return deposits.data.reduce((acc, deposit) => {
+        if (deposit.value !== null && deposit.price !== null) {
+          const usdValue = deposit.value * deposit.price;
+          acc[deposit.casino].deposited += usdValue;
+          acc[deposit.casino].score += calculateDepositScore(usdValue);
+        }
+        return acc;
+      }, initialAmounts);
+    }
+
+    return initialAmounts;
+  }, [deposits.data, deposits.isSuccess, solanaDeposits]);
 
   const totalScore = useMemo(
     () => Object.values(amounts).reduce((acc, cur) => acc + cur.score, 0),
---
+++ apps/realbet/src/app/bonus/utils/solanaDepositChecker.ts
@@ -0,0 +1,394 @@
+import { useWalletAddresses } from '@/hooks/useWalletAddresses';
+import { useQuery } from '@tanstack/react-query';
+import { z } from 'zod';
+import { getSolanaAssetMetadata } from './getSolanaTxMetadata';
+
+interface SolanaBaseTransaction {
+  from: string;
+  to: string;
+  amount: number;
+  timestamp: string;
+}
+
+interface SolanaSRCTxn extends SolanaBaseTransaction {
+  mint: string;
+  toUserAccount: string;
+  tokenStandard: string;
+  fromUserAccount: string;
+  tokenAmount: string;
+}
+
+interface Transaction {
+  timestamp: string;
+  tokenTransfers: SolanaSRCTxn[];
+  nativeTransfers: {
+    toUserAccount: string;
+    fromUserAccount: string;
+    amount: number;
+  }[];
+}
+
+interface TransactionWithValue extends SolanaBaseTransaction {
+  solAmount: number;
+  usdValue: number;
+  tokenMetadata?: {
+    symbol: string;
+    decimals: number;
+    adjustedAmount: number;
+    pricePerToken: number;
+  };
+}
+
+const CoinDataResponseSchema = z.object({
+  data: z.record(
+    z.array(
+      z.object({
+        quotes: z.array(
+          z.object({
+            quote: z.object({
+              USD: z.object({
+                price: z.number(),
+              }),
+            }),
+          }),
+        ),
+      }),
+    ),
+  ),
+});
+
+export const SHUFFLE_SOL_TREASURY_WALLET =
+  '76iXe9yKFDjGv3HicUVVy8AYxHLC71L1wYa12zaZzHHp';
+
+export const ROLLBIT_SOL_TREASURY_WALLET =
+  'RBHdGVfDfMjfU6iUfCb1LczMJcQLx7hGnxbzRsoDNvx';
+
+export const BCGAME_SOL_TREASURY_WALLET =
+  '97UQvPXbadGSsVaGuJCBLRm3Mkm7A5DVJ2HktRzrnDTB';
+
+export const BETFURY_SOL_TREASURY_WALLET =
+  '2oUVDCMTKKCDHiuMmCgZX6Vq9irR92K2vjxTxQsNNrdS';
+
+export type Casinos = 'shuffle' | 'rollbit' | 'bcgame' | 'betfury';
+
+const Cas = ['shuffle', 'rollbit', 'bcgame', 'betfury'] as const;
+
+export const getSolCasinoTreasuryWallet = (casino: Casinos) => {
+  switch (casino) {
+    case 'shuffle':
+      return SHUFFLE_SOL_TREASURY_WALLET;
+    case 'rollbit':
+      return ROLLBIT_SOL_TREASURY_WALLET;
+    case 'bcgame':
+      return BCGAME_SOL_TREASURY_WALLET;
+    case 'betfury':
+      return BETFURY_SOL_TREASURY_WALLET;
+    default:
+      return SHUFFLE_SOL_TREASURY_WALLET;
+  }
+};
+
+const GetHistoricalPriceAtTime = (symbol: string, timestamp: string) => {
+  const isoString = new Date(+timestamp * 1000).toISOString();
+  const params = new URLSearchParams({
+    symbol: symbol,
+    time_start: isoString,
+    count: '1',
+    convert: 'USD',
+  });
+
+  const { data }: { data?: number } = useQuery({
+    queryKey: ['price', timestamp, symbol],
+    queryFn: async () => {
+      const response = await fetch(`/api/coinHistoricalData?${params}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+
+      const res = CoinDataResponseSchema.parse(await response.json());
+      const quotes = res.data[symbol]?.[0]?.quotes;
+      if (quotes && quotes.length > 0) {
+        const price = quotes[0]?.quote.USD.price;
+        return price;
+      }
+    },
+    enabled: Boolean(timestamp),
+  });
+
+  if (data) {
+    return data;
+  }
+};
+
+const FetchAllTransactions = (fromAddress: string) => {
+  const data = useQuery({
+    queryKey: ['address', fromAddress],
+    queryFn: async () => {
+      const response = await fetch('/api/solanaTx', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          fromAddress: fromAddress,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+
+      const res = (await response.json()) as Promise<Transaction[]>;
+      return res;
+    },
+    enabled: fromAddress !== '',
+  });
+
+  if (data.isLoading === false && data.isSuccess) {
+    return data.data;
+  }
+};
+
+//Native transfers tx
+const SolanaNativeTransactions = (fromAddress: string) => {
+  const origin = FetchAllTransactions(fromAddress);
+
+  if (origin) {
+    const alpha = origin.flatMap((txn: Transaction) => {
+      return txn.nativeTransfers
+        .filter(
+          (transfer) =>
+            transfer.toUserAccount !== fromAddress &&
+            transfer.fromUserAccount === fromAddress,
+        )
+        .map((transfer) => ({
+          from: transfer.fromUserAccount,
+          to: transfer.toUserAccount,
+          amount: transfer.amount,
+          timestamp: txn.timestamp,
+        }));
+    });
+    return alpha;
+  }
+};
+
+const FindNativeTreasury = (fromAddress: string, treasuryAddress: string) => {
+  const alpha = SolanaNativeTransactions(fromAddress);
+
+  if (alpha !== undefined) {
+    const treasuryTxns = alpha.flatMap((txn) => {
+      const wallet2 = txn.to;
+      const tx = FetchAllTransactions(txn.to);
+
+      if (!tx) {
+        return [];
+      }
+      if (tx) {
+        return tx.flatMap((txn: Transaction) => {
+          return txn.nativeTransfers
+            .filter(
+              (transfer) =>
+                transfer.toUserAccount === treasuryAddress &&
+                transfer.fromUserAccount === wallet2,
+            )
+            .map((transfer) => ({
+              from: transfer.fromUserAccount,
+              to: transfer.toUserAccount,
+              amount: transfer.amount,
+              timestamp: txn.timestamp,
+            }));
+        });
+      }
+    });
+    return treasuryTxns;
+  }
+};
+
+const TraceSolanaDeposits = (fromAddress: string, treasuryAddress: string) => {
+  const mapTxs = FindNativeTreasury(fromAddress, treasuryAddress);
+
+  if (!mapTxs) {
+    return { deposits: [], totalUsdValue: 0 };
+  }
+
+  const deposits = mapTxs
+    .map((txn) => {
+      const price = GetHistoricalPriceAtTime('SOL', txn?.timestamp ?? '');
+
+      if (!price) {
+        return null;
+      }
+
+      if (!txn) {
+        return null;
+      }
+
+      const solAmount = txn?.amount / 10 ** 9;
+      const usdValue = solAmount * price;
+
+      return {
+        ...txn,
+        price,
+        solAmount,
+        usdValue,
+      } as TransactionWithValue;
+    })
+    .filter(Boolean) as TransactionWithValue[];
+  const totalUsdValue = deposits.reduce((acc, txn) => acc + txn.usdValue, 0);
+
+  return { deposits, totalUsdValue };
+};
+
+//SPL token transfers
+const SolanaSrc20Transactions = (fromAddress: string) => {
+  const origin = FetchAllTransactions(fromAddress);
+
+  if (origin) {
+    const beta = origin.flatMap((txn: Transaction) => {
+      return txn.tokenTransfers
+        .filter(
+          (transfer) =>
+            transfer.toUserAccount !== fromAddress &&
+            transfer.tokenStandard === 'Fungible' &&
+            transfer.fromUserAccount === fromAddress,
+        )
+        .map((transfer) => ({
+          from: transfer.fromUserAccount,
+          to: transfer.toUserAccount,
+          amount: transfer.tokenAmount,
+          timestamp: txn.timestamp,
+          mint: transfer.mint,
+        }));
+    });
+    return beta;
+  }
+};
+
+const FindSPLTreasury = (fromAddress: string, treasuryAddress: string) => {
+  const alpha = SolanaSrc20Transactions(fromAddress);
+
+  if (alpha !== undefined) {
+    const treasuryTxns = alpha.flatMap((txn) => {
+      const wallet2 = txn.to;
+      const tx = FetchAllTransactions(txn.to);
+
+      if (!tx) {
+        return [];
+      }
+      if (tx) {
+        return tx.flatMap((txn: Transaction) => {
+          return txn.tokenTransfers
+            .filter(
+              (transfer) =>
+                transfer.toUserAccount === treasuryAddress &&
+                transfer.fromUserAccount === wallet2,
+            )
+            .map((transfer) => ({
+              from: transfer.fromUserAccount,
+              to: transfer.toUserAccount,
+              amount: transfer.tokenAmount,
+              timestamp: txn.timestamp,
+              mint: transfer.mint,
+            }));
+        });
+      }
+    });
+    return treasuryTxns;
+  }
+};
+
+const TraceSPLDeposits = (fromAddress: string, treasuryAddress: string) => {
+  const mapTxs = FindSPLTreasury(fromAddress, treasuryAddress);
+
+  if (!mapTxs) {
+    return { deposits: [], totalUsdValue: 0 };
+  }
+
+  const deposits = mapTxs
+    .map((txn) => {
+      if (!txn) {
+        return null;
+      }
+      const tokenMetadata = GetSolanaTokenMetadata(txn.mint);
+
+      if (!tokenMetadata) {
+        return null;
+      }
+
+      const { pricePerToken } = tokenMetadata.priceInfo;
+      const usdValue = +txn.amount * pricePerToken;
+
+      return {
+        ...txn,
+        price: pricePerToken,
+        usdValue,
+      };
+    })
+    .filter(Boolean) as Array<{
+    from: string;
+    to: string;
+    amount: number;
+    timestamp: number;
+    mint: string;
+    price: number;
+    amountInTokens: number;
+    usdValue: number;
+  } | null>;
+  const totalUsdValue = deposits.reduce(
+    (acc, txn) => acc + (txn?.usdValue ?? 1),
+    0,
+  );
+
+  return { deposits, totalUsdValue };
+};
+
+export const GetSolanaTokenMetadata = (mintAddress: string) => {
+  const { data } = useQuery({
+    queryKey: ['tokenMetadata', mintAddress],
+    queryFn: async () => getSolanaAssetMetadata(mintAddress),
+    enabled: Boolean(mintAddress),
+    staleTime: 5 * 60 * 1000,
+  });
+
+  return data;
+};
+
+export const SolTraceAllDeposits = (userWallet: string) => {
+  return Cas.map((casino) => {
+    const treasuryWallet = getSolCasinoTreasuryWallet(casino);
+    const solDeposits = TraceSolanaDeposits(userWallet, treasuryWallet);
+    const splDeposits = TraceSPLDeposits(userWallet, treasuryWallet);
+    const combinedUsdValue =
+      solDeposits.totalUsdValue + splDeposits.totalUsdValue;
+
+    return { casino, usdDeposit: combinedUsdValue };
+  });
+};
+
+export const useSolsDeposits = () => {
+  const addresses = useWalletAddresses();
+
+  if (!addresses.solana || addresses.solana?.length === 0) {
+    return {};
+  }
+
+  return addresses.solana.reduce(
+    (acc, address) => {
+      const deposits = SolTraceAllDeposits(address);
+
+      deposits.forEach(({ casino, usdDeposit }) => {
+        if (!acc[casino]) {
+          acc[casino] = { deposited: 0, score: 0 };
+        }
+        acc[casino].deposited += usdDeposit;
+      });
+
+      return acc;
+    },
+    {} as Record<string, { deposited: number; score: number }>,
+  );
+};
