33d5d1d0c1b26a2d5fbc68e1e958b9296a2a3c32
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-12-05 11:43:13 +0700

    Set casino url env var

--- apps/rwg-dashboard-ui/src/env.js
+++ apps/rwg-dashboard-ui/src/env.js
@@ -50,11 +50,8 @@ export const env = createEnv({
       process.env.NEXT_PUBLIC_RAW_PASS_CONTRACT_ADDRESS ??
       '0x18b9db07cf194aac853daaa076d421b1dd0c75b0',
     CASINO_API_SECRET_KEY: process.env.CASINO_API_SECRET_KEY ?? 'dummy',
-    NEXT_PUBLIC_CASINO_URL: '/casino-test-linker',
-    // process.env.NEXT_PUBLIC_VERCEL_ENV === 'development' ||
-    // !process.env.NEXT_PUBLIC_VERCEL_ENV
-    //   ? '/casino-test-linker'
-    //   : 'https://realbet.io',
+    NEXT_PUBLIC_CASINO_URL:
+      process.env.NEXT_PUBLIC_CASINO_URL ?? '/casino-test-linker',
     HELIUS_API_KEY: process.env.HELIUS_API_KEY,
     SUPABASE_DB_POSTGRES_URL: process.env.SUPABASE_DB_POSTGRES_URL,
   },
